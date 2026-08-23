4b237e223effc56454b5d090a4187d8f8b29dca8
thefixer3x <thelanonasis@gmail.com>
2026-08-23 05:09:43 +0000

    chore: remove accidentally committed cli/dist build artifacts

--- cli/dist/commands/auth.d.ts
+++
@@ -1,9 +0,0 @@
-interface LoginOptions {
-    email?: string;
-    password?: string;
-    vendorKey?: string;
-    useWebAuth?: boolean;
-}
-export declare function diagnoseCommand(): Promise<void>;
-export declare function loginCommand(options: LoginOptions): Promise<void>;
-export {};
