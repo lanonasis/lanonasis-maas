e837c472b5aa97a62ce0ddc95818671ac95f286f
thefixer3x <thelanonasis@gmail.com>
2026-08-23 05:09:43 +0000

    chore: remove accidentally committed cli/dist build artifacts

--- cli/dist/commands/api-keys.d.ts
+++
@@ -1,3 +0,0 @@
-import { Command } from 'commander';
-declare const apiKeysCommand: Command;
-export default apiKeysCommand;
--- cli/dist/commands/init.d.ts
+++
@@ -1,5 +0,0 @@
-interface InitOptions {
-    force?: boolean;
-}
-export declare function initCommand(options: InitOptions): Promise<void>;
-export {};
